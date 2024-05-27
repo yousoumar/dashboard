a468da2dd0ed78eada4ae3476d214a7553bdbd57
yousoumar <oumaiyr@gmail.com>
2024-05-27 16:53:37 +0200

    feat: make the calendar dynamic

---
+++ frontend/.angular/cache/17.3.8/vite/deps/chunk-JFPDOVUJ.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=chunk-JFPDOVUJ.js.map
---
+++ frontend/src/app/calendar/calendar.component.ts
@@ -0,0 +1,42 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Calendar } from '@fullcalendar/core';
+import iCalendarPlugin from '@fullcalendar/icalendar';
+import timeGridPlugin from '@fullcalendar/timegrid';
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [NgFor, NgIf, NgClass],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.scss',
+})
+export class CalendarComponent implements OnInit {
+  ngOnInit(): void {
+    const calendarEl = document.getElementById('calendar-content')!;
+    const calendar = new Calendar(calendarEl, {
+      plugins: [timeGridPlugin, iCalendarPlugin],
+      initialView: 'timeGridWeek',
+      views: {
+        timeGridWeek: {
+          allDaySlot: false,
+        },
+      },
+      headerToolbar: {
+        left: '',
+        center: '',
+        right: '',
+      },
+      events: {
+        url: 'http://localhost:3000/calendar',
+        format: 'ics',
+      },
+      hiddenDays: [0, 6],
+      slotMinTime: '08:00:00',
+      slotMaxTime: '19:00:00',
+      height: 'auto',
+      nowIndicator: true,
+    });
+
+    calendar.render();
+  }
+}
