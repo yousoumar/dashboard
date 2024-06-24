e3785a99b69633f77a6910cc1384bde52bd85869
Youssouf Oumar <63708012+yousoumar@users.noreply.github.com>
2024-06-24 11:36:25 +0200

    feat: make the menu dynamic and update layout & styles (#31)
    Co-authored-by: aymaneo <ouraqaymane@gmail.com>

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,34 +1,50 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { KeyValuePipe, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, KeyValuePipe],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-  menu = [
-    {
-      counter: 'Grillades',
-      items: ['Poisson braisé', 'Salade de pâtes'],
-      img: 'https://img.icons8.com/ios-glyphs/90/chicken.png',
-    },
-    {
-      counter: 'Les cuistos migrateurs',
-      items: ['Poêlée de légumes', 'Lentilles'],
-      img: 'https://img.icons8.com/ios-glyphs/90/vegan-food.png',
-    },
-    {
-      counter: 'Végétarien',
-      items: ['Steak haché', 'Frites'],
-      img: 'https://img.icons8.com/ios-glyphs/90/steak.png',
-    },
-    {
-      counter: 'Accompagnements',
-      items: ['Steak haché', 'Frites'],
-      img: 'https://img.icons8.com/ios-glyphs/90/steak.png',
-    },
-  ];
+export class MenuComponent implements OnInit {
+  menu: any = {};
+  ngOnInit(): void {
+    fetch('http://localhost:3000/menu')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        data.forEach((item: any) => {
+          if (this.menu[item.pole]) {
+            this.menu[item.pole] += ', ' + item.nom;
+          } else {
+            this.menu[item.pole] = item.nom;
+          }
+        });
+      });
+  }
+  // menu = [
+  //   {
+  //     counter: 'Grillades',
+  //     items: ['Poisson braisé', 'Salade de pâtes'],
+  //     img: 'https://img.icons8.com/ios-glyphs/90/chicken.png',
+  //   },
+  //   {
+  //     counter: 'Les cuistos migrateurs',
+  //     items: ['Poêlée de légumes', 'Lentilles'],
+  //     img: 'https://img.icons8.com/ios-glyphs/90/vegan-food.png',
+  //   },
+  //   {
+  //     counter: 'Végétarien',
+  //     items: ['Steak haché', 'Frites'],
+  //     img: 'https://img.icons8.com/ios-glyphs/90/steak.png',
+  //   },
+  //   {
+  //     counter: 'Accompagnements',
+  //     items: ['Steak haché', 'Frites'],
+  //     img: 'https://img.icons8.com/ios-glyphs/90/steak.png',
+  //   },
+  // ];
 }
