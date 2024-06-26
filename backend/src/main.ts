708c3a931dabe0084f2229f708b5b9897d521e89
Youssouf Oumar <63708012+yousoumar@users.noreply.github.com>
2024-06-26 16:11:10 +0200

    chore: use dynamic port for deployment (#38)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
